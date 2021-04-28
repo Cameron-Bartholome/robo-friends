566eaa623ca6cd0e1065a2d0289f513d7be93182
Cameron Bartholome <65352273+Cameron-Bartholome@users.noreply.github.com>
2021-04-28 17:23:29 +0200

    fixed typo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 import { robots } from './robots';
-import CardList from 'CardList';
+import CardList from './CardList';
 
 ReactDOM.render(
   <CardList robots={robots} />
