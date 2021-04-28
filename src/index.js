3692178706c00fc511932a659424250fde9e2d39
Cameron Bartholome <65352273+Cameron-Bartholome@users.noreply.github.com>
2021-04-28 16:47:38 +0200

    simple change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+import Card from './Card.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+  </div>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
