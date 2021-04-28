05bc552d7a5ff6638787a97525aab2a627e61ed7
Cameron Bartholome <65352273+Cameron-Bartholome@users.noreply.github.com>
2021-04-28 18:26:44 +0200

    fixed cardlist

--- src/CardList.js
+++ src/CardList.js
@@ -3,14 +3,21 @@ import Card from './Card';
 
 
 const CardList = ({ robots }) => {
-    const cardcomponent = robots.map((user, i)=> {
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-    })
     return (
         <div>
-         {cardcomponent} 
+        {
+            robots.map((user, i)=> {
+                return (
+                    <Card key={Card.id} 
+                    id={robots[i].id} 
+                    name={robots[i].name} 
+                    email={robots[i].email} 
+                    />
+                );
+            })
+            } 
         </div>
-        );
+    );
 }
 
 export default CardList;
