65dcceba6bb7e77aa49b40ec6a4312cf21238435
Cameron Bartholome <65352273+Cameron-Bartholome@users.noreply.github.com>
2021-04-29 13:34:00 +0200

    added background and font

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
-
+import './App.css';
 
 class  App extends Component {
     constructor () {
@@ -15,19 +15,18 @@ class  App extends Component {
     }
 
     onSearchChange = (event) => {
-        this.setstate({ searchfield: event.target.value })
+        this.setState({ searchfield: event.target.value })
     }
 
     render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-
         return(
             <div className = 'tc'>
-                <h1>Robo-Friends</h1>
+                <h1 className='f1' >Robo-Friends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <CardList robots={filteredRobots} />
             </div>
         )
     }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -7,8 +7,7 @@ const SearchBox = ({searchChange}) => {
             className = 'pa3 ba b--green bg-lightest-blue'  
             type='search' 
             placeholder='Search Robots'
-            onChange={searchChange}
-            />
+            onChange={ searchChange }/>
         </div>
     );
 }
