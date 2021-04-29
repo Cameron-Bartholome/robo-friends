2b159b529a36c1aaae4666435991328173c619c7
Cameron Bartholome <65352273+Cameron-Bartholome@users.noreply.github.com>
2021-04-29 12:32:15 +0200

    added search functionality but #001 issue

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
+const state = {
+    robots : robots,
+    searchfield:''
+}
 
-const App = () => {
-    return(
-        <div className = 'tc'>
-            <h1>Robo-Friends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    )
+class  App extends Component {
+    render() {
+        return(
+            <div className = 'tc'>
+                <h1>Robo-Friends</h1>
+                <SearchBox />
+                <CardList robots={robots}/>
+            </div>
+        )
+    }
+    
 }
 
 export default App;
