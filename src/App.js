4147665c93f49e0b310cc91b0671a7eefd11b419
Cameron Bartholome <65352273+Cameron-Bartholome@users.noreply.github.com>
2021-04-29 12:32:56 +0200

    TypeError: this.setstate is not a function issue

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const state = {
-    robots : robots,
-    searchfield:''
-}
 
 class  App extends Component {
+    constructor () {
+        super()
+        this.state = {
+            robots : robots,
+            searchfield:''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setstate({ searchfield: event.target.value })
+    }
+
     render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        
         return(
             <div className = 'tc'>
                 <h1>Robo-Friends</h1>
-                <SearchBox />
-                <CardList robots={robots}/>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
             </div>
         )
     }
